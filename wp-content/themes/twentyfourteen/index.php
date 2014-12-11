6e029289e9c7bb592a0609da572746c069a0baa0
ericksantana <erickesantana@gmail.com>
2014-12-10 23:46:27 -0300

    Pag. produtos

--- wp-content/themes/twentyfourteen/index.php
+++ wp-content/themes/twentyfourteen/index.php
@@ -51,13 +51,13 @@ get_header(); ?>
 					data-large="<?php echo get_template_directory_uri(); ?>/images/banner1.png"
 					data-retina="<?php echo get_template_directory_uri(); ?>/images/banner1.png"/>
 
-				<h3 class="sp-layer sp-black sp-padding" 
+				<!--<h3 class="sp-layer sp-black sp-padding" 
 					data-horizontal="40" data-vertical="40" 
 					data-show-transition="right">
 					Lorem ipsum dolor sit amet
 				</h3>
 
-				<p class="sp-layer sp-white sp-padding" 
+				 <p class="sp-layer sp-white sp-padding" 
 					data-horizontal="40" data-vertical="100" 
 					data-show-transition="right" data-show-delay="200">
 					consectetur adipisicing elit
@@ -67,7 +67,7 @@ get_header(); ?>
 					data-horizontal="40" data-vertical="160" data-width="350" 
 					data-show-transition="right" data-show-delay="400">
 					sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				</p>
+				</p> -->
 			</div>
 
 	        <div class="sp-slide">
@@ -78,13 +78,13 @@ get_header(); ?>
 					data-large="<?php echo get_template_directory_uri(); ?>/images/img-2.png"
 					data-retina="<?php echo get_template_directory_uri(); ?>/images/img-2.png"/>
 
-				<h3 class="sp-layer sp-black sp-padding" 
+				<!--<h3 class="sp-layer sp-black sp-padding" 
 					data-horizontal="40" data-vertical="40" 
 					data-show-transition="left">
 					Lorem ipsum dolor sit amet
 				</h3>
 
-				<p class="sp-layer sp-white sp-padding" 
+				 <p class="sp-layer sp-white sp-padding" 
 					data-horizontal="40" data-vertical="100" 
 					data-show-transition="left" data-show-delay="200">
 					consectetur adipisicing elit
@@ -94,7 +94,7 @@ get_header(); ?>
 					data-horizontal="40" data-vertical="160" data-width="350" 
 					data-show-transition="left" data-show-delay="400">
 					sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				</p>
+				</p> -->
 			</div>
 
 			<div class="sp-slide">
@@ -105,7 +105,7 @@ get_header(); ?>
 					data-large="<?php echo get_template_directory_uri(); ?>/images/img-3.png"
 					data-retina="<?php echo get_template_directory_uri(); ?>/images/img-3.png"/>
 
-				<p class="sp-layer sp-white sp-padding" 
+				<!-- <p class="sp-layer sp-white sp-padding" 
 					data-position="centerCenter" data-vertical="-50" 
 					data-show-transition="right" data-show-delay="500" >
 					Lorem ipsum dolor sit amet
@@ -115,7 +115,7 @@ get_header(); ?>
 					data-position="centerCenter" data-vertical="50" 
 					data-show-transition="left" data-show-delay="700">
 					consectetur adipisicing elit
-				</p>
+				</p> -->
 			</div>
 
 			<div class="sp-slide">
@@ -126,11 +126,11 @@ get_header(); ?>
 					data-large="<?php echo get_template_directory_uri(); ?>/images/img-4.png"
 					data-retina="<?php echo get_template_directory_uri(); ?>/images/img-4.png"/>
 
-				<p class="sp-layer sp-black sp-padding"
+				<!-- <p class="sp-layer sp-black sp-padding"
 					data-position="bottomLeft" data-vertical="0" data-width="100%"
 					data-show-transition="up">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				</p>
+				</p> -->
 			</div>
 
 			<div class="sp-slide">
@@ -141,11 +141,11 @@ get_header(); ?>
 					data-large="<?php echo get_template_directory_uri(); ?>/images/img-5.png"
 					data-retina="<?php echo get_template_directory_uri(); ?>/images/img-5.png"/>
 
-				<p class="sp-layer sp-white sp-padding" 
+				<!-- <p class="sp-layer sp-white sp-padding" 
 					data-vertical="5%" data-horizontal="5%" data-width="90%" 
 					data-show-transition="down" data-show-delay="400">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				</p>
+				</p> -->
 			</div>
 
 			<div class="sp-slide">
@@ -156,10 +156,10 @@ get_header(); ?>
 					data-large="<?php echo get_template_directory_uri(); ?>/images/img-6.png"
 					data-retina="<?php echo get_template_directory_uri(); ?>/images/img-6.png"/>
 
-				<p class="sp-layer sp-white sp-padding" 
+				<!-- <p class="sp-layer sp-white sp-padding" 
 					data-horizontal="10" data-vertical="10" data-width="300">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				</p>
+				</p> -->
 			</div>
 
 			<div class="sp-slide">
@@ -170,11 +170,11 @@ get_header(); ?>
 					data-large="<?php echo get_template_directory_uri(); ?>/images/img-1.png"
 					data-retina="<?php echo get_template_directory_uri(); ?>/images/img-1.png"/>
 
-				<p class="sp-layer sp-black sp-padding" 
+				<!-- <p class="sp-layer sp-black sp-padding" 
 					data-position="bottomLeft" data-horizontal="5%" data-vertical="5%" data-width="90%" 
 					data-show-transition="up" data-show-delay="400">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				</p>
+				</p> -->
 			</div>
 
 			<div class="sp-slide">
@@ -246,7 +246,7 @@ get_header(); ?>
 		<div class="description">
 			<h2>CARINHOS BOLOS</h2>
 			<p>A Carinhos Bolos é uma empresa familiar que<br>nasceu da intenção dos integrantes de ter um<br>negócio próprio e fosse principalmente<br>prazeroso para todos...</p>
-			<a href="#"> > Conheça mais sobre nós</a>
+			<a href="?page_id=2"> > Conheça mais sobre nós</a>
 		</div>
 		<div class="news">
 			<span>NOVIDADES</span>
